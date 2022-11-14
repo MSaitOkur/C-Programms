e0d18b11f1b09aea7bac6d3c528725fef7f5aa42
Muhammed Sait Okur <muhammedsaitokur@gmail.com>
2022-11-14 08:13:09 +0200

    14Nov2022_1

--- src/main.c
+++ src/main.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
-// This macro is dangereous because there is no function overloading in C
-#define cmf_1(T) \
-    T max(T x, T y) { return x > y ? x : y; }
+// st nd rd th
+// day month year
+void print_date(int day, int month, int year)
+{
+    switch (day)
+    {
 
-// This macro is safe because this usage is not function declaration.
-#define cmf_2(T) \
-    T max_##T(T x, T y) { return x > y ? x : y; }
-
-cmf_1(int);
-/* cmf_1(double); // invalid */
-
-cmf_2(int);
-cmf_2(double); // valid
+    }
+}
 
 int main()
 {
-    printf("max(2,4) = %d\n", max(2, 4));
-    printf("max_int(2,4) = %d\n", max_int(2, 4));
-    printf("max_double(2.,4.) = %f\n", max_double(2., 4.));
+    //
 }
