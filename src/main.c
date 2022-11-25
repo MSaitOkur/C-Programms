a3adf3a1d0430b3d596986355e5e0ec2db79a101
Muhammed Sait Okur <muhammedsaitokur@gmail.com>
2022-11-25 06:53:11 +0200

    25_11_2022_1

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
+#include <limits.h>
 
 #define MAX_CMD_LINE 4096
 #define MAX_CMD_PARAMS 128
@@ -12,35 +14,43 @@ typedef struct tagCMD
     void (*proc)(void);
 } CMD;
 
-void parse_cmd_line(void);
+void parse_cmd_line(char *cmdline);
 void dir_proc(void);
 void clear_proc(void);
 void pwd_proc(void);
+void cd_proc(void);
+void exit_sys(const char *msg);
 
-char g_cmdline[MAX_CMD_LINE];
 char *g_params[MAX_CMD_PARAMS];
 int g_nparams;
+char g_cwd[PATH_MAX];
 
 CMD g_cmds[] = {
     {"dir", dir_proc},
     {"clear", clear_proc},
     {"pwd", pwd_proc},
+    {"cd", cd_proc},
     {NULL, NULL},
 };
 
 int main(void)
 {
+    char cmdline[MAX_CMD_LINE];
+
+    if (getcwd(g_cwd, PATH_MAX) == NULL)
+        exit_sys("getcwd");
+
     while (1)
     {
-        fprintf(stdout, "CSD>");
-        if (fgets(g_cmdline, MAX_CMD_LINE, stdin) == NULL)
+        fprintf(stdout, "CSD:%s> ", g_cwd);
+        if (fgets(cmdline, MAX_CMD_LINE, stdin) == NULL)
             continue;
 
         char *str;
-        if ((str = strchr(g_cmdline, '\n')) != NULL)
+        if ((str = strchr(cmdline, '\n')) != NULL)
             *str = '\0';
 
-        parse_cmd_line();
+        parse_cmd_line(cmdline);
 
         if (g_nparams == 0)
             continue;
@@ -61,12 +71,12 @@ int main(void)
     }
 }
 
-void parse_cmd_line(void)
+void parse_cmd_line(char *cmdline)
 {
     g_nparams = 0;
 
     char *str;
-    for (str = strtok(g_cmdline, " \t"); str; str = strtok(NULL, " \t"))
+    for (str = strtok(cmdline, " \t"); str; str = strtok(NULL, " \t"))
         g_params[g_nparams++] = str;
 }
 
@@ -84,11 +94,39 @@ void pwd_proc(void)
 {
     if (g_nparams > 1)
     {
-        fprintf(stderr, "pwd command mustn't be used with argument...\n");
+        fprintf(stderr, "ERR: pwd command mustn't be used with argument...\n");
+        return;
+    }
+    fprintf(stdout, "%s\n", g_cwd);
+}
+
+void cd_proc(void)
+{
+    char *dir;
+    if (g_nparams > 2)
+    {
+        fprintf(stdout, "ERR: cd too many arguments!...\n");
         return;
     }
 
-    char cwd[4096];
-    getcwd(cwd, 4096);
-    fprintf(stdout, "%s\n", cwd);
+    if (g_nparams == 1)
+    {
+        if ((dir = getenv("HOME")) == NULL)
+            exit_sys("Fatal error(getenv)");
+    }
+    else
+        dir = g_params[1];
+
+    if (chdir(dir) == -1)
+        fprintf(stderr, "%s", strerror(errno));
+
+    if (getcwd(g_cwd, PATH_MAX) == NULL)
+        exit_sys("Fatal error(getcwd)");
+}
+
+void exit_sys(const char *msg)
+{
+    perror(msg);
+
+    exit(EXIT_FAILURE);
 }
